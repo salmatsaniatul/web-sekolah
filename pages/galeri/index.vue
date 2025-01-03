252fb2eededf1e39f806b2f29e4bad9fbf241994
salmatsaniatul <141211517+salmatsaniatul@users.noreply.github.com>
2025-01-04 05:48:28 +0700

    Add files via upload

---
+++ pages/galeri/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container-fluid">
+    <h2 class="text-center mt-4">Galeri</h2>
+
+    <!--MPLS-->
+    <div class="card mt-5">
+      <h2 class="text-center mt-3">Upacara 17 Agustus 2024</h2>
+      <div class="row justify-content-center m-3">
+        <div class="col-md-4">
+          <div class="card">
+            <img
+              src="~/assets/galeri/1.png"
+              alt="galeri"
+            />
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img
+              src="~/assets/galeri/2.png"
+              alt="galeri"
+            />
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img
+              src="~/assets/galeri/3.png"
+              alt="galeri"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="row justify-content-center m-3">
+        <div class="col-md-4">
+          <div class="card">
+            <img
+              src="~/assets/galeri/4.png"
+              alt="galeri"
+            />
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img
+              src="~/assets/galeri/5.png"
+              alt="galeri"
+            />
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card">
+            <img
+              src="~/assets/galeri/6.png"
+              alt="galeri"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container-fluid {
+  margin-top: 115px;
+}
+
+img {
+  height: 300px;
+}
+</style>
